8168c7ceb18968664f827c2b504a01ceb8bb05f5
simpleappcreator <simpleappcreator@gmail.com>
2015-11-11 04:16:10 +0530

    gulp watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,20 +2,27 @@ const gulp = require('gulp');
 const del = require('del');
 const babel = require('gulp-babel');
 const replace = require('gulp-replace');
+const changed = require('gulp-changed-in-place');
+const util = require('gulp-util');
 
 gulp.task('clean', done =>
-    del('lib', done));
+    del('lib/**/*.*', done));
 
 gulp.task('copy', done =>
     gulp.src('src/**/*.*')
     .pipe(gulp.dest('lib')));
 
+var babelSrc = ['src/**/*.js', '!src/client/public/vendor/**/*.js'];
 gulp.task('babel', done =>
-    gulp.src(['src/**/*.js', '!src/client/public/vendor/**/*.js'])
-    .pipe(babel({
-        presets: ['es2015', 'stage-0']
-    }))
+    gulp.src(babelSrc)
+    .pipe(changed({firstPass: true}))
+    .pipe(babel({presets: ['es2015', 'stage-0']}))
     .pipe(replace(/(\/\/ )?["']use strict['"];?([\n\r]+)?/g, ''))
     .pipe(gulp.dest('lib')));
 
-gulp.task('default', gulp.series('clean', 'copy', 'babel'));
+gulp.task('build', gulp.series('clean', 'copy', 'babel'));
+
+gulp.task('watch', done =>
+    gulp.watch(babelSrc, gulp.series('babel')));
+
+gulp.task('default', gulp.series('build', 'watch'));
