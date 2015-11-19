e1a1660d4d3e3619e9de002f9c990c87885c7d0b
simpleappcreator <simpleappcreator@gmail.com>
2015-11-20 00:41:19 +0530

    sourcemaps

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,22 @@
+require('unclog')('p');
 const gulp = require('gulp');
 const babel = require('gulp-babel');
 const replace = require('gulp-replace');
+const sourcemaps = require('gulp-sourcemaps');
 const changed = require('gulp-changed-in-place');
 
-process.title = 'Gulp: Simple-app';
+process.title = 'Gulp: ' + cwd;
 
 gulp.task('babel', done =>
     gulp.src('**/*.es6')
     .pipe(changed({firstPass: true}))
+    .pipe(sourcemaps.init())
     .pipe(babel({
         presets: ['es2015', 'stage-0'],
         retainLines: 'true',
     }))
     .pipe(replace(/(\/\/ )?["']use strict['"];?([\n\r]+)?/g, ''))
+    .pipe(sourcemaps.write('.', { sourceRoot: '.' }))
     .pipe(gulp.dest('.')));
 
 
